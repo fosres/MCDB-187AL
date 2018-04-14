ebaf0887ae91ca020483b8e349ffcdb3127d1cac
Tanveer Salim <tanveerasalim@gmail.com>
2018-04-14 10:04:07 -0700

    MCDB187-UPDATED

---
+++ MCDB 187AL/git_save.sh
@@ -0,0 +1,13 @@
+# git_save.sh
+# Saves git local repository for MCDB187AL
+# to GitHub
+
+#!/bin/bash
+
+	cd ~/Desktop/MCDB-187AL/MCDB\ 187AL/
+
+	git add .
+
+	git commit -m MCDB187-UPDATED
+
+	git push origin MCDB187-UPDATED
